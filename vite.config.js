ee8f95001a40bf5dfd796a12cd5fb5efec6cba00
shubhodeep sen <87843331+shubhodeep786@users.noreply.github.com>
2023-07-15 09:07:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+})
