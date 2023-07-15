d483910bdd4b4640ef78278b11cd9923eadfaafe
shubhodeep sen <87843331+shubhodeep786@users.noreply.github.com>
2023-07-15 08:59:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['/src/main.jsx'],
-    },
-  },
 });
